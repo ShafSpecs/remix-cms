f27cdede0c6d6b3ef25a087a837bd737789f48b0
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-03-04 00:30:13 +0100

    Integrating Octokit

--- app/utils/server/github.server.ts
+++ app/utils/server/github.server.ts
@@ -1,3 +1,10 @@
 import { Octokit } from "@octokit/core";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+(async () => {
+  await octokit.request("GET /repos/{owner}/{repo}", {
+    owner: "ShafSpecs",
+    repo: "remix-cms",
+  });
+})();
